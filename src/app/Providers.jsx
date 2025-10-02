afad9a000c6f25588eaebded7fe4d73bd1e518d9
Shaminfarhan farhan <shaminfarhanfarhan@gmail.com>
2025-10-02 12:42:46 -0500

    issue fix

--- src/app/Providers.jsx
+++ src/app/Providers.jsx
@@ -2,15 +2,13 @@
 
 import { SessionProvider } from "next-auth/react";
 import { CurrencyProvider } from "./CurrencyContext";
-import { WishlistProvider } from "@/components/shared/context/WishlistContext";
+
 
 
 export default function Providers({ children }) {
   return (
     <SessionProvider>
-    <WishlistProvider>
       <CurrencyProvider>{children}</CurrencyProvider>
-      </WishlistProvider>
     </SessionProvider>
   );
 }
--- src/app/layout.js
+++ src/app/layout.js
@@ -3,7 +3,7 @@ import "./globals.css";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Providers from "./Providers";
-
+import { WishlistProvider } from "@/components/shared/context/WishlistContext";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +27,9 @@ export default function RootLayout({ children }) {
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <ToastContainer />
+        <WishlistProvider>
         <Providers>{children}</Providers>
+        </WishlistProvider>
       </body>
     </html>
   );
