564e619f2c7f0159f2bc00f7efddf8a863365bf4
Shaminfarhan farhan <shaminfarhanfarhan@gmail.com>
2025-11-07 09:36:18 -0500

    image optimization issue fixed

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-    domains: ['montres.ae','res.cloudinary.com',"lh3.googleusercontent.com","platform-lookaside.fbsbx.com","unoptimized: true"]
+  images: {
+    domains: [
+      'montres.ae',
+      'res.cloudinary.com',
+      'lh3.googleusercontent.com',
+      'platform-lookaside.fbsbx.com',
+    ],
+    unoptimized: true, // ✅ placed correctly outside of domains array
   },
- 
+
   env: {
     NEXT_PUBLIC_BASEURL: process.env.NEXT_PUBLIC_BASEURL,
   },
