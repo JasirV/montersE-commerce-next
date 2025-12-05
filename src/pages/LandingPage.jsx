50fb789deceabbcd1b05b1fdfe9719a8e5ab2250
Montres Trading L.L.C – The Art Of Time <admin@montres.ae>
2025-12-05 06:00:22 -0500

    Update LandingPage.jsx

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -54,7 +54,7 @@ export default function IndexPage() {
       />
       <Landing />
       <div className="flex md:hidden justify-center">
-        {/* <CategorySectionWithImages/> */}
+       <CategorySectionWithImages/> 
       </div>
       <Home />
       <WatchBrand />
