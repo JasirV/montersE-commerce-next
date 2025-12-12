d95b194e73abb2956a528e69c4f5646f9c14fbe5
Shaminfarhan farhan <shaminfarhanfarhan@gmail.com>
2025-12-12 09:58:33 -0500

    clodudinary issue

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -52,7 +52,7 @@ export default function IndexPage() {
       />
       <Landing />
       <Home />
-      <WatchBrand />
+      {/* <WatchBrand /> */}
       <Form id="watch-form" />
       <PremiumBrands />
       <Watch />
